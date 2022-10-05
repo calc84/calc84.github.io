f44f5d96840ba1b0c80dc2580978dac25815a2f5
Stanley Moukhametzianov <stanleymoukh@gmail.com>
2022-10-05 13:47:23 -0400

    changed graph

--- screen.js
+++ screen.js
@@ -1,12 +1,40 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let width = 400;
+    let height = 400;
+
+
+    functionPlot({
+        target: "#root",
+        width,
+        height,
+        disableZoom: true,
+        yAxis: {
+            domain: [-10, 10]
+        },
+        xAxis: {
+            domain: [-10, 10]
+        },
+        grid: true,
+        fnType: 'implicit',
+        data: [{
+            fn: "sin(x)",
+            closed: false
+        }, {
+            fn: "x^3",
+            closed: false
+        }, ]
+    });
+
+}, false);
 
 let valueCurr = '';
 
 var evalinput = function () {
-document.getElementById('input').value  = valueCurr;
- var output=document.getElementById('output');
- var input=document.getElementById('input');
+    document.getElementById('input').value = valueCurr;
+    var output = document.getElementById('output');
+    var input = document.getElementById('input');
     UI.eval_input(output, input);
-    
+
 };
 
 var clickDigit = function (uniqueValue) {
